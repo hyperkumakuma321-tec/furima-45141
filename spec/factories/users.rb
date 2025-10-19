bad50c99419997b539de16cb8f0fb7241969f51b
hyperkumakuma321-tec <marbletyokotec@gmail.com>
2025-10-19 18:22:28 +0900

    単体テスト　新規登録ユーザー情報

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.email}
+    password              {Faker::Internet.password(min_length: 6, mix_case: true, special_characters: false)}
+    password_confirmation {password}
+    first_name            {Faker::Japanese::Name.first_name}
+    last_name             {Faker::Japanese::Name.last_name}
+    kana_first_name       {"ヒトシ"}
+    kana_last_name        {"クマキ"}
+    birth                 {"1990-03-21"}
+  end
+end
